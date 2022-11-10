031687bdf8df8478acbdf4f7bd75defa25a9ff97
unknown <parvezmrf@gmail.com>
2022-11-10 18:32:32 +0600

    404 updated

--- src/Pages/NotFound/NotFound.js
+++ src/Pages/NotFound/NotFound.js
@@ -3,7 +3,7 @@ import React from 'react';
 const NotFound = () => {
     return (
         <div>
-            Sorry. 404 Not Found
+            Sorry. 404 Not Found. Back to home Please.
         </div>
     );
 };

4e9c95d2675d980af1428cc855177f2b19e0cea8
unknown <parvezmrf@gmail.com>
2022-11-11 12:51:10 +0600

    rating added

--- src/Pages/Home/Home/RecentReview/RecentReview.js
+++ src/Pages/Home/Home/RecentReview/RecentReview.js
@@ -139,8 +139,8 @@ const RecentReview = () => {
                             <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
                             <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
                             <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
-                            <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
                             <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" checked />
+                            <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
                         </div>
                     </div>
 
